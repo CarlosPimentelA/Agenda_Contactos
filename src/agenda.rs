90e110ed9c67bc912544800055659b85f5647779
carlospimentel.a <157527363+CarlosPimentelA@users.noreply.github.com>
2025-06-07 21:21:23 -0400

    Implementacion de la funcion para guardar contactos. TODO: guardar los contactos en un archivo JSON.

--- src/agenda.rs
+++ src/agenda.rs
@@ -1,25 +1,87 @@
-pub mod agenda {
-    pub fn agenda(eleccion: u8) {
-        match eleccion {
-            1 => añadir_contacto(),
+use std::io;
 
-            2 => editar_contacto(),
+use uuid::Uuid;
 
-            3 => borrar_contacto(),
+pub fn agenda(eleccion: u8) {
+    match eleccion {
+        1 => añadir_contacto(),
 
-            _ => print!("Porfavor seleccione el numero de la opcion indicada"),
-        }
+        2 => editar_contacto(),
+
+        3 => borrar_contacto(),
+
+        4 => mostrar_contactos(),
+
+        5 => println!("Saliste!"),
+
+        _ => print!("Porfavor seleccione una opcion valida (1 - 4)"),
     }
+}
 
-    fn añadir_contacto() {
-        print!("Añadir");
-    }
+fn añadir_contacto() {
+    print!("\x1B[2J\x1B[1;1H");
+    println!("Coloque el nombre del contacto: ");
+    let mut nombre = String::new();
+    io::stdin()
+        .read_line(&mut nombre)
+        .expect("error: unable to read user input");
+    print!("\x1B[2J\x1B[1;1H");
 
-    fn editar_contacto() {
-        print!("Añadir")
-    }
+    print!("\x1B[2J\x1B[1;1H");
+    println!("Coloque el apellido del contacto: ");
 
-    fn borrar_contacto() {
-        print!("Añadir")
-    }
+    let mut apellido = String::new();
+    io::stdin()
+        .read_line(&mut apellido)
+        .expect("error: unable to read user input");
+
+    print!("\x1B[2J\x1B[1;1H");
+    println!("Coloque el telefono del contacto: ");
+    let mut telefono = String::new();
+    io::stdin()
+        .read_line(&mut telefono)
+        .expect("error: unable to read user input");
+
+    let contacto = Contacto {
+        nombre: nombre.trim().to_string(),
+        apellido: apellido.trim().to_string(),
+        numero_telefono: telefono.trim().to_string(),
+        _id: Uuid::new_v4(),
+    };
+
+    println!(
+        "Nombre: {}, Apellido: {}, Telefono: {}, ID: {}",
+        contacto.nombre, contacto.apellido, contacto.numero_telefono, contacto._id
+    );
+    mostrar_opciones();
+}
+
+fn editar_contacto() {
+    mostrar_opciones();
+    println!("Editar");
+}
+
+fn borrar_contacto() {
+    mostrar_opciones();
+    println!("Borrar");
+}
+
+fn mostrar_contactos() {
+    mostrar_opciones();
+    println!("Contactos")
+}
+
+#[derive(Debug)]
+pub struct Contacto {
+    nombre: String,
+    apellido: String,
+    numero_telefono: String,
+    _id: Uuid,
+}
+
+fn mostrar_opciones() {
+    print!("\x1B[2J\x1B[1;1H");
+    print!(
+        "Que desea hacer (Seleccione el numero de la opcion deseada): \n1. Añadir contacto\n2. Editar contacto\n3. Borrar contacto.\n4. Mostrar contactos\n5. Salir.\n"
+    );
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 mod agenda;
-use agenda::agenda::agenda;
+use agenda::agenda;
 use std::io;
 fn main() {
     print!(
-        "Que desea hacer (Seleccione el numero de la opcion deseada): \n1. Añadir contacto\n2. Editar contacto\n3. Borrar contacto.\n"
+        "Que desea hacer (Seleccione el numero de la opcion deseada): \n1. Añadir contacto\n2. Editar contacto\n3. Borrar contacto.\n4. Mostrar contactos\n5. Salir.\n"
     );
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("error: unable to read user input");
 
-    let mut input = String::new();
-    io::stdin()
-        .read_line(&mut input)
-        .expect("error: unable to read user input");
-    let eleccion = input
-        .replace("\r", "")
-        .replace("\n", "")
-        .parse::<u8>()
-        .unwrap();
+        let input = match input.trim().parse::<u8>() {
+            Ok(input) => input,
+            Err(_) => {
+                println!("Coloque un valor valido (1 - 4)");
+                continue;
+            }
+        };
 
-    agenda(eleccion);
+        agenda(input);
+        if input == 5 {
+            break;
+        }
+    }
 }
